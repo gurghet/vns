16a8145015c4d899018e6667c9efbc90925bd497
gurghet <gurghet@gmail.com>
2013-03-28 14:35:42 +0100

    Rinominato prova in leggimi.txt, commentato il Main

--- vns/src/vnsBFP/Main.java
+++ vns/src/vnsBFP/Main.java
@@ -3,11 +3,16 @@ package vnsBFP;
 public class Main {
 
 	/**
-	 * @param args
+	 * @param nome file istanza, numero macchine, numero mosse
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// leggi il file istanza e crea gli oggetti Jobs
+		
+		// crea una soluzione iniziale vuota
+		
+		// fai girare l'algoritmo
 
+		// stampa risultato su un file
 	}
 
 }
