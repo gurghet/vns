ff875beefc05986c0a951c2ad5fc4abc30e89d95
gurghet <gurghet@gmail.com>
2013-03-31 11:39:08 +0200

    Aggiunta la classe job

---
+++ vns/src/vnsBFP/Job.java
@@ -0,0 +1,12 @@
+package vnsBFP;
+
+public class Job {
+	public int id;
+	public int procTime;
+	public int dueDate;
+	public int weight;
+	/*
+	 * Bisogna valutare se e come inserire qui i set-up times, ad
+	 * esempio in un array e i vincoli di precedenza
+	 * */
+}
